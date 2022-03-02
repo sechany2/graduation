c06d21de15d61c2845621b3b5089d99b96b2a8a3
minju kim <alswn1348@naver.com>
2022-03-02 17:17:41 +0900

    navigation view prototype

--- app/src/main/java/com/example/graduation/MainActivity.java
+++ app/src/main/java/com/example/graduation/MainActivity.java
@@ -1,14 +1,74 @@
 package com.example.graduation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private View drawerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerView = (View) findViewById(R.id.drawer);
+
+        Button btn_menu = (Button)findViewById(R.id.btn_menu);      //메뉴버튼
+        btn_menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               drawerLayout.openDrawer(drawerView);
+            }
+        });
+        Button btn_close = (Button)findViewById(R.id.btn_close);    //닫기버튼
+        btn_close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.closeDrawers();
+            }
+        });
+        drawerLayout.setDrawerListener(listener);
+        drawerView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                return true;
+            }
+        });
+
     }
-}
+
+        DrawerLayout.DrawerListener listener = new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+
+            }
+
+            @Override
+            public void onDrawerOpened(@NonNull View drawerView) {  //메뉴오픈시
+
+            }
+
+            @Override
+            public void onDrawerClosed(@NonNull View drawerView) {  //닫았을때
+
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+
+            }
+        };
+
+    }
+
