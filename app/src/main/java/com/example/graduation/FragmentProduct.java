816ec442d354e71e7fc11e5b471b02e77d52cf40
Sechan Jang <tpcks1268@gmail.com>
2022-04-28 15:47:30 +0900

    0428 제품별점구현

--- app/src/main/java/com/example/graduation/FragmentProduct.java
+++ app/src/main/java/com/example/graduation/FragmentProduct.java
@@ -7,14 +7,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class FragmentProduct extends Fragment {
@@ -24,6 +31,10 @@ public class FragmentProduct extends Fragment {
     private TextView tv_pdbrandname;
     private ImageView iv_profile;
     private String pd_code;
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
+    double avg = 0;
+    public RatingBar ratingbar;
 
     @Nullable
     @Override
@@ -45,6 +56,56 @@ public class FragmentProduct extends Fragment {
             Log.e("번들 이동 실패","오류");
         }
 
+        pd_code = new String();
+        productinfo = getArguments().getStringArrayList("product");
+        if (getArguments() != null) {
+            pd_code = productinfo.get(10);
+        }
+        else{
+            Log.e("번들 이동 실패","오류");
+        }
+
+        database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference("Review");
+        List pdscore = new ArrayList<>();
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                // 클래스 모델이 필요?
+                for (DataSnapshot fileSnapshot : dataSnapshot.getChildren()) {
+                    //MyFiles filename = (MyFiles) fileSnapshot.getValue(MyFiles.class);
+                    //하위키들의 value를 어떻게 가져오느냐???
+                    if (fileSnapshot.child(pd_code).getValue(Double.class) != null){
+                        String aaa = fileSnapshot.child(pd_code).getValue(Double.class).toString();
+                        Log.e("value is ", aaa);
+                        pdscore.add(aaa);
+                    }
+                }
+                Log.e("pdscore리스트",pdscore.toString());
+                Log.e("pdscore리스트",pdscore.get(0).toString());
+
+                String qwe = null;
+                double sum = 0;
+
+                for(int i = 0; i < pdscore.size(); i++){
+                    qwe = pdscore.get(i).toString();
+                    sum = sum + Double.parseDouble(qwe);
+                    avg = sum / pdscore.size();
+                }
+                System.out.println("평균은 : "+avg);
+
+                ratingbar = view.findViewById(R.id.pd_ratingbar);
+                ratingbar.setRating((float) avg);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w("TAG: ", "Failed to read value", databaseError.toException());
+            }
+        });
+
         //정보분석 버튼
         Button btn_info = (Button)view.findViewById(R.id.btn_infome);
         btn_info.setOnClickListener(new View.OnClickListener() {
@@ -58,15 +119,6 @@ public class FragmentProduct extends Fragment {
                 ((MainActivity)getActivity()).replaceframeProduct(fragmentInfo);  //메인 엑티비티에 프래그먼트 이동 메소드 호출
             }
         });
-
-        pd_code = new String();
-        productinfo = getArguments().getStringArrayList("product");
-        if (getArguments() != null) {
-            pd_code = productinfo.get(10);
-        }
-        else{
-            Log.e("번들 이동 실패","오류");
-        }
 
         //리뷰 버튼
         Button btn_review = (Button)view.findViewById(R.id.btn_review);
