44cd4bb09a7b7cf6f1c3adcfc553533ac4a95a39
alswn <alswn1348@naver.com>
2022-06-06 16:52:54 +0900

    문의 하기 분석 요청하기 기능 추가

--- app/src/main/java/com/example/graduation/Fragments/Fragment_full_request.java
+++ app/src/main/java/com/example/graduation/Fragments/Fragment_full_request.java
@@ -1,10 +1,17 @@
 package com.example.graduation.Fragments;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.Layout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,7 +22,9 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.graduation.Adapter.MyReviewAdapter;
 import com.example.graduation.Adapter.RequestAdapter;
 import com.example.graduation.Object.Request;
+import com.example.graduation.Object.UserAccount;
 import com.example.graduation.R;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -37,22 +46,53 @@ public class Fragment_full_request extends Fragment {
     private DatabaseReference databaseReference;
     private ArrayList<Request> arrayList;
     private RecyclerView recyclerView;
-    private RecyclerView.Adapter adapter;
+    private RequestAdapter adapter;
     private RecyclerView.LayoutManager layoutManager;
+    private String name;
+    private FirebaseAuth mAuth;
+    private ArrayList<String> arrayList3;
+    private int flag;
+    private boolean clickFlag = true;
+    private String answer ;
+    private Context ct;
 
+
+
+    private AlertDialog.Builder dlg ;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_full_request, container, false);
+        ct = container.getContext();
 
         database = FirebaseDatabase.getInstance();
-
+        mAuth = FirebaseAuth.getInstance();
         recyclerView = view.findViewById(R.id.rv_request);
         recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false);
         recyclerView.setLayoutManager(layoutManager);
         arrayList = new ArrayList<>();
         arrayList.clear();
+        databaseReference = database.getReference();
+        arrayList3 = new ArrayList<>();
+        ValueEventListener uavalueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //파이어베이스에서 본인 정보 저장
+                for (DataSnapshot snapshot2 : snapshot.getChildren()) {
+                    UserAccount userAccount = snapshot2.getValue(UserAccount.class);
+                    if (snapshot2.getKey().equals(mAuth.getUid())) {
+                        name = userAccount.getName();       //이름 저장
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e("error", error.toString());
+            }
+        };
+        databaseReference.child("graduation").child("UserAccount").addValueEventListener(uavalueEventListener);
 
         databaseReference = database.getReference("Request");
         ValueEventListener RequestEventListener = new ValueEventListener() {
@@ -61,10 +101,10 @@ public class Fragment_full_request extends Fragment {
                 //파이어베이스 데이터베이스의 데이터를 받아오는 곳
                 //pdarrayList.clear(); // 기존 배열리스트가 존재하지않게 초기화
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) { // 반복문으로 데이터 List를 추출해냄
-
-                        Request request = snapshot.getValue(Request.class);
-                        request.setCategory("분석 요청");
-                        arrayList.add(request);
+                    arrayList3.add(snapshot.getKey());
+                    Request request = snapshot.getValue(Request.class);
+                    request.setCategory("분석 요청");
+                    arrayList.add(request);
 
                 }
 
@@ -86,14 +126,91 @@ public class Fragment_full_request extends Fragment {
                 //파이어베이스 데이터베이스의 데이터를 받아오는 곳
                 //pdarrayList.clear(); // 기존 배열리스트가 존재하지않게 초기화
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) { // 반복문으로 데이터 List를 추출해냄
-
-
-                        Request request = snapshot.getValue(Request.class);
-                        request.setCategory("질의 문의");
-                        arrayList.add(request);
+                    flag = arrayList3.size();
+                    arrayList3.add(snapshot.getKey());
+                    Request request = snapshot.getValue(Request.class);
+                    request.setCategory("질의 문의");
+                    arrayList.add(request);
 
                 }
+
+
+                ArrayList<Request> arrayList2 = new ArrayList<>();
+
+
+                if (name.equals("admin")) {
+                    adapter = new RequestAdapter(arrayList, getContext());
+                    adapter.setOnItemClickListener(new RequestAdapter.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(View v, int pos) {
+
+
+                                LinearLayout rqlayout = v.findViewById(R.id.answer);
+                                rqlayout.setVisibility((View.VISIBLE));
+                            if (clickFlag) {
+                                Button a_btn = v.findViewById(R.id.answer_btn);
+                                EditText tv_rq_answer = v.findViewById(R.id.tv_rq_answer);
+                                tv_rq_answer.setEnabled(true);
+
+                                databaseReference = database.getReference();
+                                a_btn.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        if (pos < flag) {
+
+                                            dlg = new AlertDialog.Builder(ct);
+                                            dlg.setTitle("답변 완료");
+                                            dlg.setPositiveButton("확인",new DialogInterface.OnClickListener(){
+                                                @Override
+                                                public void onClick(DialogInterface dialogInterface, int i) {
+                                                    answer = new String();
+                                                    answer = tv_rq_answer.getText().toString();
+                                                    databaseReference.child("Request").child(arrayList3.get(pos)).child("answer").setValue(answer);
+                                                    tv_rq_answer.setEnabled(false);
+                                                    a_btn.setVisibility(View.GONE);
+                                                }
+                                            });
+                                            dlg.show();
+                                        } else {
+                                            dlg = new AlertDialog.Builder(ct);
+                                            dlg.setTitle("답변 완료");
+                                            dlg.setPositiveButton("확인",new DialogInterface.OnClickListener(){
+                                                @Override
+                                                public void onClick(DialogInterface dialogInterface, int i) {
+                                                    answer = new String();
+                                                    answer = tv_rq_answer.getText().toString();
+                                                    databaseReference.child("Inquiry").child(arrayList3.get(pos)).child("answer").setValue(answer);
+                                                    tv_rq_answer.setEnabled(false);
+                                                    a_btn.setVisibility(View.GONE);
+                                                }
+                                            });
+                                            dlg.show();
+
+                                        }
+                                    }
+                                });
+                                if(tv_rq_answer.getText().toString().equals("")) {
+                                    clickFlag = false;
+                                }
+                            }
+                            else {
+                                rqlayout.setVisibility(View.GONE);
+                                clickFlag =true;
+                            }
+                        }
+                    });
+                } else {
+                    for (Request snapshot : arrayList) {
+                        if (snapshot.getName().equals(name)) {
+                            arrayList2.add(snapshot);
+                        }
+                    }
+                    adapter = new RequestAdapter(arrayList2, getContext());
+                }
+
+                recyclerView.setAdapter(adapter);
                 adapter.notifyDataSetChanged(); // 리스트 저장 및 새로고침
+
             }
 
             @Override
@@ -102,10 +219,10 @@ public class Fragment_full_request extends Fragment {
                 Log.e("MainActivity", String.valueOf(databaseError.toException())); // 에러문 출력
             }
         };
+        databaseReference = database.getReference("Inquiry");
         databaseReference.addValueEventListener(InquiryEventListener);
 
-        adapter = new RequestAdapter(arrayList, getContext());
-        recyclerView.setAdapter(adapter);
+
         return view;
     }
 
--- app/src/main/java/com/example/graduation/Fragments/Fragmentmy.java
+++ app/src/main/java/com/example/graduation/Fragments/Fragmentmy.java
@@ -12,6 +12,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.graduation.Activity.MainActivity;
+import com.example.graduation.Object.Request;
 import com.example.graduation.Object.UserAccount;
 import com.example.graduation.R;
 import com.google.firebase.auth.FirebaseAuth;
@@ -30,9 +31,10 @@ public class Fragmentmy extends Fragment {
     private FirebaseAuth mAuth;
     private ArrayList<Object> arrayList2;
     public Fragmentmy(){ }
-    String name;
-    String rvcount="0";
-
+    public String name;
+    public String rvcount="0";
+    public String rqcount="0";
+    public int count  = 0;
     public static Fragmentmy newInstance() {
         return new Fragmentmy();
     }
@@ -79,6 +81,35 @@ public class Fragmentmy extends Fragment {
                     }
                 };
                 databaseReference.child("Review").child(name).addValueEventListener(rvwtvalueEventListener);
+                ValueEventListener rqEventListener = new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        //파이어베이스에서 본인 정보 저장
+
+                        for (DataSnapshot snapshot2 : snapshot.getChildren()) {
+                            if (snapshot2.getValue() != null) {
+                                Request request = new Request();
+                                request =  snapshot2.getValue(Request.class);
+
+                                if(request.getName().equals(name)){
+                                    count++;
+                                }
+                                rqcount = String.valueOf(count);
+                            }
+                        }
+                        TextView tv_rq_count = view.findViewById(R.id.tv_rq_count);
+                        tv_rq_count.setText(rqcount);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                };
+                count =0;
+                databaseReference.child("Request").addValueEventListener(rqEventListener);
+                databaseReference.removeEventListener(rqEventListener);
+                databaseReference.child("Inquiry").addValueEventListener(rqEventListener);
             }
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
@@ -92,6 +123,15 @@ public class Fragmentmy extends Fragment {
             public void onClick(View view) {
                 Fragmentmyreview fragmentmyreview = new Fragmentmyreview();
                 ((MainActivity)getActivity()).replaceFragment(fragmentmyreview); //메인 엑티비티에 프래그먼트 이동 메소드 호출
+            }
+        });
+
+        TextView my_rq_btn = view.findViewById(R.id.tv_rq_btn);
+        my_rq_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment_full_request fragmentfr = new Fragment_full_request();
+                ((MainActivity)getActivity()).replaceFragment(fragmentfr); //메인 엑티비티에 프래그먼트 이동 메소드 호출
             }
         });
 
