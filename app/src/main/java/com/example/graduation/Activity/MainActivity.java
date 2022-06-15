ad3f23158687239967b40a32872c1c6cafde9479
Sechan Jang <tpcks1268@gmail.com>
2022-06-15 11:19:28 +0900

    인기가많은제품5개만 보이기

--- app/src/main/java/com/example/graduation/Activity/MainActivity.java
+++ app/src/main/java/com/example/graduation/Activity/MainActivity.java
@@ -365,7 +365,9 @@ public class MainActivity extends AppCompatActivity {
                 arrayList.clear(); // 기존 배열리스트가 존재하지않게 초기화
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) { // 반복문으로 데이터 List를 추출해냄
                     Product product = snapshot.getValue(Product.class);// 만들어뒀던 Product 객체에 데이터를 담는다.
-                    arrayList.add(product); // 담은 데이터들을 배열리스트에 넣고 리사이클러뷰로 보낼 준비
+                    if(arrayList.size()<5){
+                        arrayList.add(product); // 담은 데이터들을 배열리스트에 넣고 리사이클러뷰로 보낼 준비
+                    }
                 }
                 adapter.notifyDataSetChanged(); // 리스트 저장 및 새로고침
             }
