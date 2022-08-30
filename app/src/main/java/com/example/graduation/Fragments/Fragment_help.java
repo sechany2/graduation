d748297bc9c91449d28e04aa51e89cf88f6108e7
K406-04\USER <bcucs20@gmail.com>
2022-08-30 14:54:46 +0900

    파이어베이스 최신버전으로 업데이트 중 도움말 기능 추가 중
    현재 문제
    파이어베이스 리얼타임데이터베이스 연결오류
    xml id 인식 오류

--- app/src/main/java/com/example/graduation/Fragments/Fragment_help.java
+++ app/src/main/java/com/example/graduation/Fragments/Fragment_help.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -17,14 +18,36 @@ public class Fragment_help extends Fragment {
         Fragment_help fragment = new Fragment_help();
         return fragment;
     }
-
+    private TextView help1,help2,help3;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fargment_help, container, false);
 
+        help1 = view.findViewById(R.id.tv_help1);
+        help1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+            }
+        });
+
+        help2 = view.findViewById(R.id.tv_help2);
+        help2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        help3 = view.findViewById(R.id.tv_help3);
+        help3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
         return  view;
     }
 
